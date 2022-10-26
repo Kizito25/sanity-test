67bf37079e2f2f6e4fadfb9851951ccdd05207b2
devKizito <devkizito@gmail.com>
2022-10-26 17:05:53 +0100

    Fixed unused imports - Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
 import { IoMdAdd, IoMdSearch } from "react-icons/io";
 
 const Navbar = ({ searchTerm, setSearchTerm, user }) => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   if (!user) return null;
   return (
