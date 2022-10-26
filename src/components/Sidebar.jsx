550de4361a4a3d30be9932786172cb1a038375fc
devKizito <devkizito@gmail.com>
2022-10-26 17:03:23 +0100

    Fixed unused imports

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { RiHomeFill } from "react-icons/ri";
-import { IoIosArrowForward } from "react-icons/io";
+// import { IoIosArrowForward } from "react-icons/io";
 
 import logo from "../assets/logo.png";
 
